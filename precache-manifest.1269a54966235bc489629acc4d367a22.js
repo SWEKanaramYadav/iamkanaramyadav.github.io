4e8dbcf0e3e2c48002cf3f926cb74e67fe9db506
kana ram yadav <kanaramyadavofficial@gmail.com>
2021-03-14 15:05:23 +0530

    Updates

---
+++ precache-manifest.1269a54966235bc489629acc4d367a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f986a778dac8ef776d8674eb79fee6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0a85bf524c694ce11f",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8c839529b06db7db94a6",
+    "url": "/static/js/2.c343569a.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.c343569a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0a85bf524c694ce11f",
+    "url": "/static/js/main.dea8e765.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
